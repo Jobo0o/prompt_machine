96af1f8c97ffd4eaa320fce6107cb59bfa57e6b0
Johannes Schenk <johannesschenk@LCE50469.corp.capgemini.com>
2023-08-02 14:08:03 +0200

    style & conversation history

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from flask import Flask, request, render_template
-import os
 import openai
+import os
 
-# Initialize a Flask application
 app = Flask(__name__)
 
-# Define the route for the application's home page
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def home():
     # Initialize the variables
-    title = instruction = examples = target_task = output_format = api_response = None
- 
-    # Initialize the API response variable
-    api_response = None
+    title = instruction = examples = target_task = output_format = api_response = generated_prompt = None
+    conversation = []
+    display_conversation = []
 
     # If the request method is 'POST', the form has been submitted
     if request.method == 'POST':
@@ -28,20 +23,26 @@ def home():
         # Get the OpenAI API key from the environment variables
         openai.api_key = os.getenv('OPENAI_API_KEY')
 
+        # Store the user's message in the conversation history
+        display_conversation.append({"role": "user", "content": f"Title: {title}\nInstruction: {instruction}\nExamples: {examples}\nTarget Task: {target_task}\nDesired Output Format: {output_format}"})
+
         # Prepare the conversation for the OpenAI API to generate a prompt
         conversation = [
-            {"role": "system", "content": "You are a helpful assistant. Use the following details to create an improved prompt. Only give out the new prompt and nothing else."},
+            {"role": "system", "content": "You are a helpful assistant. Use the following details to create an improved prompt."},
             {"role": "user", "content": f"Title: {title}\nInstruction: {instruction}\nExamples: {examples}\nTarget Task: {target_task}\nDesired Output Format: {output_format}"}
         ]
 
         # Send a request to the OpenAI API to generate a prompt
         response = openai.ChatCompletion.create(
-          model="gpt-3.5-turbo",
-          messages=conversation
+            model="gpt-3.5-turbo",
+            messages=conversation
         )
 
         # Extract the generated prompt from the API response
         generated_prompt = response.choices[0].message["content"]
+
+        # Store the AI's message (improved prompt) in the conversation history
+        display_conversation.append({"role": "prompt", "content": generated_prompt})
 
         # Prepare the conversation for the OpenAI API to generate a response based on the generated prompt
         conversation = [
@@ -50,12 +51,18 @@ def home():
 
         # Send a request to the OpenAI API to generate a response based on the generated prompt
         response = openai.ChatCompletion.create(
-          model="gpt-3.5-turbo",
-          messages=conversation
+            model="gpt-3.5-turbo",
+            messages=conversation
         )
 
-        # Store the API response
+        # Extract the AI's response from the API response
         api_response = response.choices[0].message["content"]
 
-    # Render the home page with the form and the API response
-    return render_template('home.html', title=title, instruction=instruction, examples=examples, target_task=target_task, output_format=output_format, api_response=api_response)
+        # Store the AI's message (response) in the conversation history
+        display_conversation.append({"role": "assistant", "content": api_response})
+
+    # Render the home page with the form and the conversation history
+    return render_template('home.html', title=title, instruction=instruction, examples=examples, target_task=target_task, output_format=output_format, api_response=api_response, conversation=display_conversation)
+
+if __name__ == '__main__':
+    app.run(debug=True)
